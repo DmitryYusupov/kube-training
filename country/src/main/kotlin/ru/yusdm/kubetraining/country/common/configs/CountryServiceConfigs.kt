0d6bd4be22a615e28e18976cdbccc349614c1d04
DmitryYusupov <usikovich@mail.ru>
2018-10-03 17:09:49 +0300

    Changed deployment file : added env props to make feign workable

--- country/src/main/kotlin/ru/yusdm/kubetraining/country/common/configs/CountryServiceConfigs.kt
+++ country/src/main/kotlin/ru/yusdm/kubetraining/country/common/configs/CountryServiceConfigs.kt
@@ -2,5 +2,5 @@ package ru.yusdm.kubetraining.country.common.configs
 
 
 object CountryServiceConfigs {
-    const val CITY_KUBERNETES_SERVICE = "city-srv"
+    const val CITY_KUBERNETES_SERVICE = "citysrvfeign"
 }
