96213aa63056e7df3cea572217fba6a42615129e
DmitryYusupov <usikovich@mail.ru>
2018-10-05 15:05:46 +0300

    Changed template to run POD which is responsible for country unit

--- country/src/main/kotlin/ru/yusdm/kubetraining/country/common/configs/YamlConfigs.kt
+++ country/src/main/kotlin/ru/yusdm/kubetraining/country/common/configs/YamlConfigs.kt
@@ -1,9 +1,20 @@
 package ru.yusdm.kubetraining.country.common.configs
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class YamlConfigs {
-    var cityService: String = "dssd"
-    val ambassadorService: String = "sss"
+
+    companion object {
+        const val SERVICE_PREFIX = "custom.services."
+        const val CITY_SERVICE_PREFIX = "${SERVICE_PREFIX}cityservice."
+    }
+
+    @Value("\${" + CITY_SERVICE_PREFIX + "logicalName}")
+    lateinit var cityService: String
+
+    @Value("\${" + SERVICE_PREFIX + "ambassadorservice.logicalName}")
+    lateinit var ambassadorService: String
 }
+
