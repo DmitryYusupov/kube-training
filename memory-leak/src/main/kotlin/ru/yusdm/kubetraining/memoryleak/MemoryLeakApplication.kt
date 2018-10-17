0b7b4a3a36d023614c0278e5492a2f4b6e97f576
DmitryYusupov <usikovich@mail.ru>
2018-10-17 15:09:28 +0300

    some changes

--- memory-leak/src/main/kotlin/ru/yusdm/kubetraining/memoryleak/MemoryLeakApplication.kt
+++ memory-leak/src/main/kotlin/ru/yusdm/kubetraining/memoryleak/MemoryLeakApplication.kt
@@ -9,9 +9,8 @@ fun main(args: Array<String>) {
     var counter = 0
     while (true) {
         list.add("Test str to call memory leak! ")
-        if (counter++ % 500000 == 0) {
+        if (counter++ % 5000000 == 0) {
             println("Memory leak in progress. Size of list with strings is '${list.size}'")
-            //Thread.sleep(400)
         }
     }
 
